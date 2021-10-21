a8919844013fb07474fdfd8423974c15554184fe
Anthony Chen <anthonychen33@gmail.com>
2021-10-21 15:53:38 -0400

    Started the form for adding books to library

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let myLibrary = []; 
+const addBookButton = document.querySelector(".newBook-Button");
+const formContainer = document.querySelector(".form-container"); 
 
-function Book(title, author) {
+function Book(title, author, read) {
     this.title = title; 
     this.author = author;
+    this.read = read; 
     // the constructor
 }
 
@@ -18,16 +21,27 @@ function printLibrary() {
     }
 }
 
-const bookObject = new Book("Clean Code", "Robert Cecil Martin"); 
-const secondBook = new Book("The Pragmatic Programmer", "Andy Hunt and Dave Thomas");
-const thirdBook = new Book("Code: The Hidden Language of Computer Hardware and Software", "Charles Petzold");
+function createForm() {
+    console.log("Creating form...");
+    // Book Name input
+    const inputBookName = document.createElement("input");
+    inputBookName.classList.add("input-Book");
+    inputBookName.placeholder = "Book Name";
+    const inputBookAuthor = document.createElement("input"); 
+    inputBookAuthor.classList.add("input-Book");
+    inputBookAuthor.placeholder = "Author";
+    formContainer.appendChild(inputBookName);
+    formContainer.appendChild(inputBookAuthor);   
+    
+}
 
-myLibrary[0] = bookObject; 
-myLibrary[1] = secondBook; 
-myLibrary[2] = thirdBook; 
+addBookButton.addEventListener("click", createForm);
+//myLibrary[0] = bookObject; 
+//myLibrary[1] = secondBook; 
+//myLibrary[2] = thirdBook; 
 
 //console.log(myLibrary[0].title + ", " + myLibrary[0].author); 
 console.table(myLibrary); 
 
 
-printLibrary(); 
+//printLibrary(); 
